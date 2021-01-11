207d742eb13d1b1e1864103ac9a099501ad74ca0
Zach <zbayard@gmail.com>
2021-01-10 20:13:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+//for ([initialization]; [condition]; [iteration]) {
+    //[loop body]
+  //} 
+
+const arrayOfNames = ["Mai", "Zach", "Eric"]
+const eventName = "surprise"
+
+function writeCards(arrayOfNames, event){
+    let arr2 = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        arr2.push(`Thank you, ${arrayOfNames[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return arr2;
+}
+
+
+
+function countDown(num) {
+    //let i = num
+    while (num >= 0){
+        console.log(num--)
+    }
+}
